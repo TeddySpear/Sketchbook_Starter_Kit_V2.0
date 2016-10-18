639deb923c5ce21f2f8a5dc58e294ae3617c3772
TeddySpear <teddy@pennpals.us>
2016-10-17 21:02:42 -0400

    Commit by TeddySpear

---
+++ Teddy/Teddy.ino
@@ -0,0 +1,49 @@
+// ctrl-shift-b to build
+// ctrl-shift-p, run tasks, --upload to run on arduino
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 0;
+const int colorG = 255;
+const int colorB = 0;
+
+const int pinTemp = A0;
+
+// Define the B-value of the thermistor.
+// This value is a property of the thermistor used in the Grove - Temperature Sensor,
+// and used to convert from the analog value it measures and a temperature value.
+const int B = 3975;
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+
+}
+
+void loop() 
+{
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 0);
+    // Get the (raw) value of the temperature sensor.
+    int val = analogRead(pinTemp);
+
+    // Determine the current resistance of the thermistor based on the sensor value.
+    float resistance = (float)(1023-val)*10000/val;
+
+    // Calculate the temperature based on the resistance value.
+    float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
+    temperature = temperature * 9.0 / 5.0 + 32;
+    // Print the temperature to the serial console.
+    lcd.print(temperature);
+
+    // Wait one second between measurements.
+    delay(1000);
+    
+}
